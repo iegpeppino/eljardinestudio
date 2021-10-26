5d017335c9e1a6a18f5aead6f20481e10157d352
iegpeppino <84358776+iegpeppino@users.noreply.github.com>
2021-10-26 20:34:46 -0300

    Path to favicon added

--- jardin/urls.py
+++ jardin/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jardinpage.urls')),
+    path('favicon.ico',RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
