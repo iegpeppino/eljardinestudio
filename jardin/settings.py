dd39a7d7776205daf328127c5aee072bb3f978b8
iegpeppino <84358776+iegpeppino@users.noreply.github.com>
2021-10-26 15:35:47 -0300

    Modified Settings and Url

--- jardin/settings.py
+++ jardin/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zekjy+$w(2mmh1(ygc4=60-_ptuf*2zhu!+=795855ctoa6mp8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','eljardinestudio.herokuapp.com']
 
 
 # Application definition
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,9 +138,9 @@ GS_BUCKET_NAME = 'eljardinestudio'
 STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 STATIC_URL = 'https://storage.googleapis.com/eljardinestudio/'
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = 'static/media'
+MEDIA_ROOT = 'static/media'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
